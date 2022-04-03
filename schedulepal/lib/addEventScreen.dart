7720abf06035e55f7b2f2e7e0dc1f36c91107caa
Emir Ibrisimovic <emiribrisimovicc@gmail.com>
2022-04-03 15:57:14 -0400

    spicy add event

--- schedulepal/lib/addEventScreen.dart
+++ schedulepal/lib/addEventScreen.dart
@@ -17,11 +17,19 @@ class AddEventScreen extends StatefulWidget {
 
 class _AddEventScreenState extends State<AddEventScreen> {
   // Project's Firebase authentication instance
+  var location = "";
+  late DateTime date;
+  late DateTime savedDate;
+  late DateTime startTime;
+  late DateTime savedStartTime;
+  late DateTime endTime;
+  late DateTime savedEndTime;
   final FirebaseAuth auth = FirebaseAuth.instance;
   final FirebaseFirestore store = FirebaseFirestore.instance;
 
   final newEventNameController = TextEditingController();
   final newEventDescriptionController = TextEditingController();
+  final newEventLocationController = TextEditingController();
 
   @override
     Widget build(BuildContext context) {
@@ -40,7 +48,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
             ),
           ),
           child: Card(
-            margin: const EdgeInsets.only(top: 155, bottom: 75, left: 30, right: 30),
+            margin: const EdgeInsets.only(top: 145, bottom: 75, left: 30, right: 30),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -104,12 +112,38 @@ class _AddEventScreenState extends State<AddEventScreen> {
                             )
                         )
                     ),
+                    Center(
+                        child: RaisedButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) => showEvents(context),
+                              );
+                            },
+                            child: Text(
+                              'curr events',
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            )
+                        )
+                    ),
                     ElevatedButton(
                         onPressed: () async {
-                          var userID = auth.currentUser?.uid;
-                          var userRef = await store.collection("User").doc(userID);
-                          await userRef.update({'events': newEventNameController.text});
-                          goBack(context);
+                            var userID = auth.currentUser?.uid;
+                            var userRef = await store.collection("User").doc(userID);
+                            final CollectionReference eventsRef = FirebaseFirestore.instance.collection('/Events');
+                            eventsRef.add({'name' : newEventNameController.text,
+                                           'description' : newEventDescriptionController.text,
+                                           'location': location,
+                                           'date' : savedDate,
+                                           'startTime' : savedStartTime,
+                                           'endTime' : savedEndTime})
+                                .then((value) {
+                                  var list = [value.id];
+                                  userRef.update({"events": FieldValue.arrayUnion(list)});
+                            });
+                            goBack(context);
                           },
                         child: Text(
                             "Save",
@@ -143,9 +177,9 @@ Widget chooseDateAndTime(BuildContext context) {
           padding: const EdgeInsets.fromLTRB(0, 0, 0, 300),
           child: CupertinoDatePicker(
             mode: CupertinoDatePickerMode.date,
-            initialDateTime: DateTime(2021, 1, 1, 11, 33),
+            initialDateTime: DateTime(2021, 1, 1),
             onDateTimeChanged: (DateTime newDateTime) {
-              //Do Some thing
+              date = newDateTime;
             },
             use24hFormat: false,
             minuteInterval: 1,
@@ -179,7 +213,7 @@ Widget chooseDateAndTime(BuildContext context) {
                 mode: CupertinoDatePickerMode.time,
                 initialDateTime: DateTime(2021, 1, 1, 11, 33),
                 onDateTimeChanged: (DateTime newDateTime) {
-                  //Do Some thing
+                  startTime = newDateTime;
                 },
                 use24hFormat: false,
                 minuteInterval: 1,
@@ -193,7 +227,7 @@ Widget chooseDateAndTime(BuildContext context) {
                 mode: CupertinoDatePickerMode.time,
                 initialDateTime: DateTime(2021, 1, 1, 11, 33),
                 onDateTimeChanged: (DateTime newDateTime) {
-                  //Do Some thing
+                  endTime = newDateTime;
                 },
                 use24hFormat: false,
                 minuteInterval: 1,
@@ -207,6 +241,10 @@ Widget chooseDateAndTime(BuildContext context) {
     actions: <Widget>[
       new FlatButton(
         onPressed: () {
+          //actually save values
+          savedDate = date;
+          savedStartTime = startTime;
+          savedEndTime = endTime;
           Navigator.of(context).pop();
         },
         textColor: Theme.of(context).primaryColor,
@@ -247,7 +285,7 @@ Widget chooseLocation(BuildContext context) {
             width: 400,
             padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
             child: TextFormField(
-              // controller: newEmailController,
+              controller: newEventLocationController,
               decoration: const InputDecoration(
                 border: UnderlineInputBorder(),
                 labelText: 'Address',
@@ -259,6 +297,7 @@ Widget chooseLocation(BuildContext context) {
       actions: <Widget>[
         new FlatButton(
           onPressed: () async {
+            location = newEventLocationController.text;
             Navigator.of(context).pop();
           },
           textColor: Theme.of(context).primaryColor,
@@ -287,6 +326,101 @@ Widget chooseLocation(BuildContext context) {
         ),
       ],
     );
+  }
+
+  AlertDialog showEvents(BuildContext context) {
+    final result = store.collection('Events');
+    int cnt = 0;
+
+
+
+    return new AlertDialog(
+      title: const Text('Events List'),
+      content: Container(
+        height: 300.0, // Change as per your requirement
+        width: 300.0, // Change as per your requirement
+        child: FutureBuilder(
+          future: getEvents(),
+          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              var cnt = ','.allMatches(snapshot.data!["events"].toString()).length + 1;
+              var list = [];
+              final result = store.collection('Events');
+
+              // return store.collection("Events").doc(firebaseUser).get();
+              for (int i = 0; i < cnt; i++) {
+                result.get().then((ds) {
+                  for (var doc in ds.docs) {
+                    if (doc.id == snapshot.data!["events"][i]) {
+                      print(doc.get("name"));
+                      list.add(doc.get("name"));
+                    }
+                  }
+                  });
+                };
+              return ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: cnt,
+                itemBuilder: (BuildContext context, int index) => Column(
+                  children: <Widget>[
+                    Text(snapshot.data!["events"][index]),
+                    //store.collection("Events").doc(snapshot.data!["events"][index].toString()).get()
+                    Text(grabField(snapshot.data!["events"][index].toString())),
+                  ],
+              ),
+
+              );
+                  return Text(snapshot.data!["events"][0]);
+
+            } else if (snapshot.connectionState == ConnectionState.none) {
+              return Text("No data");
+            }
+            return CircularProgressIndicator();
+          }
+        ),
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () async {
+            location = newEventLocationController.text;
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              child: Text(
+                "Save",
+              ),
+            ),
+          ),
+        ),
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              child: Text(
+                "Cancel",
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<DocumentSnapshot> getEvents() async{
+    var firebaseUser = await auth.currentUser?.uid;
+    return store.collection("User").doc(firebaseUser).get();
+  }
+
+  String grabField(String par) {
+
+    return "";
   }
 
 Future<void> goBack(BuildContext context) async {
